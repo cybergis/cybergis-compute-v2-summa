fe7f574b31c69c647a40aac81ccbaff3df737145
Zhiyu(Drew) Li <zyli2004@gmail.com>
2021-11-29 19:48:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# import os
-# import pprint as pp
-# from distutils.dir_util import copy_tree
-# from mpi4py import MPI
-
-# # init mpi
-# comm = MPI.COMM_WORLD
-# rank = comm.Get_rank()
-# size = comm.Get_size()
-# hostname = MPI.Get_processor_name()
-
-# # all processes to print Hello World
-# print("Hello World ! main.py {}/{}@{}".format(rank, size, hostname))
-
-
 
 import json
 import os
