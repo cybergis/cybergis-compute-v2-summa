61f083f026b443a59e36d528f77152f21e425e9a
Zhiyu(Drew) Li <zyli2004@gmail.com>
2021-12-07 08:57:34 -0600

    Update and rename setup.py to preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,5 @@
+# single process for preprocessing
+# current implementation of summa in cybergis-compute does not need preprocessing
+# keep as a placeholder for later use
+
+print("preprocessing")
