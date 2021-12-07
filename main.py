5a199426aa496bf0de92f45e0dd9aa139ffd8915
Zhiyu(Drew) Li <zyli2004@gmail.com>
2021-12-07 09:13:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+# load libs
 import json
 import os
 from pathlib import Path
@@ -7,37 +8,44 @@ import numpy as np
 from mpi4py import MPI
 import subprocess
 import pysumma as ps
-# init mpi
+# init mpi env
 comm = MPI.COMM_WORLD
+# rank (0-based index for each mpi process)
 rank = comm.Get_rank()
+# size of mpi cluster
 size = comm.Get_size()
 hostname = MPI.Get_processor_name()
 print("{}/{}: {}".format(rank, size, hostname))
 
-
+# path to "executable" folder inside container
 job_folder_path = os.environ["executable_folder"]
 instance = "summa"
+# path to "data" folder inside container
 instance_path = os.environ["data_folder"]
 
+# rank 0 to create a "workers" folder in the "executable" folder
 workers_folder_name = "workers"
 workers_folder_path = os.path.join(job_folder_path, workers_folder_name)
 if rank == 0:
    os.system("mkdir -p {}".format(workers_folder_path))
 comm.Barrier()
-# copy instance folder to workers folder
+# each rank to create a <instance>_<X> in "workers" folder and copy model files
 new_instance_path = os.path.join(workers_folder_path, instance + "_{}".format(rank))
 os.system("cp -rf {} {}".format(instance_path, new_instance_path))
-# each process to call install.sh to localize SUMMA model
+# each rank to call installTest_local.sh to localize SUMMA model
+# make sure installTest_local.sh has the right permission to run
 subprocess.run(
     ["chmod", "+x", "./installTestCases_local.sh"], cwd=new_instance_path,
 )
 subprocess.run(
     ["./installTestCases_local.sh"], cwd=new_instance_path,
 )
+# chech if "summa_options.json" exists (contains parameters for ensemble runs)
 json_path = os.path.join(new_instance_path, "summa_options.json")
 ensemble_flag = True
 if not os.path.isfile(json_path):
     ensemble_flag = False
+# replace path place holders (<PWD>, <BASEDIR>, BASEDIR) in "summa_options.json" with real values inside container
 try:
     with Path(json_path) as f:
         f.write_text(f.read_text().replace('<PWD>', new_instance_path)
@@ -49,6 +57,7 @@ try:
 except Exception as ex:
     print("{}/{}: Error in parsing summa_options.js: {}".format(rank, size, ex))
     options_dict = {}
+ 
 # group config_pairs
 options_list = [(k,v) for k,v in options_dict.items()]
 options_list.sort()
@@ -65,6 +74,8 @@ print("API submitted file_manager {}".format(file_manager))
 executable = "/usr/bin/summa.exe"
 #if len(config_pair_list) == 0:
 #    config_pair_list = [("_test", {})]
+
+# each rank to process the ensemble run assigned
 for config_pair in config_pair_list:
     try:
         name = config_pair[0]
