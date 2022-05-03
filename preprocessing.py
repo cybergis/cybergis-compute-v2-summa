c9d0e04c4ea5a6c81947c1ed59b20c9ad35d4626
Zhiyu(Drew) Li <zyli2004@gmail.com>
2022-05-03 16:06:42 -0500

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,6 @@
 import os
 SLURM_JOB_ID = os.environ.get("SLURM_JOB_ID", "")
-prinrt("SLURM_JOB_ID: {}".format(SLURM_JOB_ID))
+print("SLURM_JOB_ID: {}".format(SLURM_JOB_ID))
 
 # single process for preprocessing
 # current implementation of summa in cybergis-compute does not need preprocessing
